941de9b5125616b8dd7daa63a90e8b9cb347495d
hideveloper20 <electronicmade9064@outlook.com>
2020-11-07 13:04:13 -0500

    UI design implemented

--- navigation/AppStack.js
+++ navigation/AppStack.js
@@ -1,14 +1,114 @@
 import React from 'react';
+import {View, TouchableOpacity, Text} from 'react-native';
 import {createStackNavigator} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/HomeScreen';
+import ChatScreen from '../screens/ChatScreen';
+import ProfileScreen from '../screens/ProfileScreen';
+import AddPostScreen from '../screens/AddPostScreen';
+import FontAwesome from 'react-native-vector-icons/FontAwesome5';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
-const Stack = createStackNavigator();
+const HomeStack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const HomeStackScreen = ({navigation}) => {
+  return (
+    <HomeStack.Navigator>
+      <HomeStack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          headerTitleAlign: 'center',
+          headerTitleStyle: {
+            color: '#2e64e5',
+            fontSize: 18,
+          },
+          headerStyle: {
+            shadowColor: '#fff',
+            elevation: 0,
+          },
+          headerRight: () => (
+            <View style={{marginRight: 10}}>
+              <FontAwesome.Button
+                name="plus"
+                size={22}
+                backgroundColor="#fff"
+                color="#2e64e5"
+                onPress={() => navigation.navigate('AddPost')}
+              />
+            </View>
+          ),
+        }}
+      />
+      <HomeStack.Screen
+        name="AddPost"
+        component={AddPostScreen}
+        options={{
+          title: '',
+          headerTitleAlign: 'center',
+          headerStyle: {
+            backgroundColor: '#2e64e515',
+            shadowColor: '#2e64e515',
+            elevation: 0,
+          },
+          headerBackTitleVisible: false,
+          headerBackImage: () => (
+            <View style={{marginLeft: 15}}>
+              <Ionicons name="arrow-back" size={25} color="#2e64e5" />
+            </View>
+          ),
+          headerRight: () => (
+            <View style={{marginRight: 15}}>
+              <TouchableOpacity>
+                <Text
+                  style={{fontWeight: '600', fontSize: 18, color: '#2e64e5'}}>
+                  Post
+                </Text>
+              </TouchableOpacity>
+            </View>
+          ),
+        }}
+      />
+    </HomeStack.Navigator>
+  );
+};
 
 const AppStack = () => {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="Home" component={HomeScreen} />
-    </Stack.Navigator>
+    <Tab.Navigator initialRouteName="Home">
+      <Tab.Screen
+        name="Home"
+        component={HomeStackScreen}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({color}) => (
+            <FontAwesome name="home" color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Messages"
+        component={ChatScreen}
+        options={{
+          tabBarLabel: 'Messages',
+          tabBarIcon: ({color}) => (
+            <FontAwesome name="comments" color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Home3"
+        component={ProfileScreen}
+        options={{
+          tabBarLabel: 'Profile',
+          tabBarIcon: ({color}) => (
+            <FontAwesome name="user" color={color} size={26} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
   );
 };
 
